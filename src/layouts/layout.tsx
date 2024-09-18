c228bb3087299d0f851eb46d1695dc3f9881c9ed
Rudransh Chouksey <choukseyrudransh22@gmail.com>
2024-09-18 14:27:36 +0530

    order status page

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -5,20 +5,17 @@ import Hero from "../components/Hero"
 type Props = {
     children: React.ReactNode;
     showHero?: boolean;
-}
-
-const Layout = ({ children, showHero = false }: Props) => {
+  };
+  
+  const Layout = ({ children, showHero = false }: Props) => {
     return (
-        <div className="flex flex-col min-h-scrreen">
-            <Header />
-            {showHero && <Hero />}
-            <div className="container mx-auto flex-1 py-10">
-                {children}
-            </div>
-            <Footer />
-        </div>
-    )
-
-}
-
-export default Layout
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        {showHero && <Hero />}
+        <div className="container mx-auto flex-1 py-10">{children}</div>
+        <Footer />
+      </div>
+    );
+  };
+  
+  export default Layout;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './global.css'
-import { BrowserRouter as Router } from 'react-router-dom'
-import AppRoutes from './AppRoutes'
-import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { Toaster } from './components/ui/sonner'
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./global.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Toaster } from "sonner";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,17 +14,17 @@ const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
     },
   },
-})
+});
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <Router>
       <QueryClientProvider client={queryClient}>
         <Auth0ProviderWithNavigate>
           <AppRoutes />
-          <Toaster visibleToasts={1} position='top-right' richColors />
+          <Toaster visibleToasts={1} position="top-right" richColors />
         </Auth0ProviderWithNavigate>
       </QueryClientProvider>
     </Router>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
